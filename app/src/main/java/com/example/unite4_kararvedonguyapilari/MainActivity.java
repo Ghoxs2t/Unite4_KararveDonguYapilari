0c7e1360a1edec59684129dacd89a4d46169a2ae
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-21 12:14:47 +0300

    no message

---
+++ app/src/main/java/com/example/unite4_kararvedonguyapilari/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.unite4_kararvedonguyapilari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnUyg1Goster (View view) {
+        Intent i = new Intent(MainActivity.this, Uyg1Activity.class);
+        startActivity(i);
+    }
+
+    public void btnUyg2Goster(View view)
+    {
+        Intent i = new Intent(MainActivity.this, Uyg2Activity.class);
+        startActivity(i);
+    }
+
+
+}
